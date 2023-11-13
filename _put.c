ffaaa9ae3937537557af130c3419c5c5249153b0
Muhammad Hussein <32427243+muhammadSWE@users.noreply.github.com>
2023-11-13 20:35:13 +0200

    final touches

--- _put.c
+++ _put.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * _puts - prints a string with newline
- * @str: the string to print
+ *_puts - prints a string with newline
+ *@str: the string to print
  *
- * Return: void
+ *Return: void
  */
 int _puts(char *str)
 {
@@ -13,26 +12,4 @@ int _puts(char *str)
 	while (*str)
 		_putchar(*str++);
 	return (str - a);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(int c)
-{
-	static int i;
-	static char buf[OUTPUT_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
 }
